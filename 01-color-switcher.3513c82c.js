365f7f97ebaa2543f0d8d7345304fe2fa64b2e2d
Irina-anat <116644910+Irina-anat@users.noreply.github.com>
2023-04-12 16:42:16 +0000

    Deploying to gh-pages from @ Irina-anat/goit-js-hw-09@fb0d0ad2e56897c45b885cac828431d24b59ebb7 🚀

---
+++ 01-color-switcher.3513c82c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");t.addEventListener("click",(function(){r=setInterval((()=>{let r=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;d.style.backgroundColor=r,t.setAttribute("disabled","disabled"),e.removeAttribute("disabled")}),1e3)})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled","disabled")}));let r=null;
+//# sourceMappingURL=01-color-switcher.3513c82c.js.map
