fbd3189e40a3b65f9f263007555a92c569c1301c
Iryna Rozmaita <iriszechka@ukr.net>
2023-04-16 20:01:19 +0300

    03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import '../css/03-promises.css';
+const form = document.querySelector(`.form`)
+
+form.addEventListener(`submit`, onSubmit)
+
+function onSubmit(evn) {
+  evn.preventDefault();
+
+  let delay = Number(evn.currentTarget.delay.value);
+  const step = Number(evn.currentTarget.step.value);
+  const amount = Number(evn.currentTarget.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 }
 
+function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
 
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+      } else {
+          reject({ position, delay });
+      }
+      }, delay)
+    });
+  }
+
+
+/*Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер промісу (position), що створюється, і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
+  
+Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або відхиляється через delay часу. Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay зі значеннями однойменних параметрів. Використовуй початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.*/
